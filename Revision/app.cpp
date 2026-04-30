7e9981ebdf4586cea416c2920391c9374a00b384
ashu <ashutoshvishwakarma222@gmail.com>
2026-04-30 10:42:52 +0530

    entering number till multiple of 10

--- Revision/app.cpp
+++ Revision/app.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<climits>
 using namespace std;
 
 int main(){
@@ -72,15 +73,29 @@ int main(){
 
 
     // Print the digit of a number in reverse
+    // int n;
+    // cout<<"Enter number: ";
+    // cin>>n;
+    // int rev = 0;
+    // while(n>0){
+    //     int digit = n % 10;
+    //     rev = rev * 10 + digit;
+    //     n /= 10;
+    // }
+    // cout<<"Reverse = "<<rev<<endl;
+
+
+
+    // WAP where user keeps entering the number till they enter multiple of 10
     int n;
     cout<<"Enter number: ";
-    cin>>n;
-    int rev = 0;
-    while(n>0){
-        int digit = n % 10;
-        rev = rev * 10 + digit;
-        n /= 10;
+    while(true){
+        if(n % 10 == 0){
+            break;
+        } else {
+            cin>>n;
+        }
     }
-    cout<<"Reverse = "<<rev<<endl;
+
     return 0;
 }
