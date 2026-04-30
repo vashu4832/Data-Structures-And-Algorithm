bc0880b6868db34d8d2fa048d21bf01ff571f63a
ashu <ashutoshvishwakarma222@gmail.com>
2026-04-30 09:20:47 +0530

    summ of digit

--- Revision/app.cpp
+++ Revision/app.cpp
@@ -12,14 +12,41 @@ int main(){
     // }
 
     // Print sum of first N natural numbers
+    // int n;
+    // cout<<"Enter number: ";
+    // cin>>n;
+    // int sum = 0;
+    // for(int i=1;i<=n;i++){
+    //     sum += i;
+    // }
+    // cout<<"Sum till "<<n<<" is "<<sum<<endl;
+
+    //////////////////////////////// WHILE LOOP /////////////////////////////////////////////////////
+    // int count = 1;
+    // while(count<=10){
+    //     cout<<count<<" ";
+    //     count++;
+    // }
+
+    // Print number from N to 1
+    // int n;
+    // cout<<"Enter number: ";
+    // cin>>n;
+    // for(int i=n;i>=1;i--){
+    //     cout<<i<<" ";
+    // }
+
+    // Print the sum of digit of a number
     int n;
     cout<<"Enter number: ";
     cin>>n;
     int sum = 0;
-    for(int i=1;i<=n;i++){
-        sum += i;
+    while(n>0){
+        int digit = n % 10;
+        sum += digit;
+        n /= 10;
     }
-    cout<<"Sum till "<<n<<" is "<<sum<<endl;
+    cout<<"Sum of digit is "<<sum<<endl;
 
     return 0;
 }
