38decebeb5bbb10309d1b67455f1e1648bbdd8ce
ashu <ashutoshvishwakarma222@gmail.com>
2026-04-30 09:32:09 +0530

    print digits in reverse order

--- Revision/app.cpp
+++ Revision/app.cpp
@@ -3,13 +3,14 @@ using namespace std;
 
 int main(){
     // Print first N natural numbers
-
     // int n;
     // cout<<"Enter number: ";
     // cin>>n;
     // for(int i=1;i<=n;i++){
     //     cout<<i<<" ";
     // }
+
+
 
     // Print sum of first N natural numbers
     // int n;
@@ -28,6 +29,8 @@ int main(){
     //     count++;
     // }
 
+
+
     // Print number from N to 1
     // int n;
     // cout<<"Enter number: ";
@@ -35,6 +38,8 @@ int main(){
     // for(int i=n;i>=1;i--){
     //     cout<<i<<" ";
     // }
+
+
 
     // Print the sum of digit of a number
     // int n;
@@ -48,20 +53,33 @@ int main(){
     // }
     // cout<<"Sum of digit is "<<sum<<endl;
 
+
+
     // Print the sum of Odd digit of a Number
+    // int n;
+    // cout<<"Enter number: ";
+    // cin>>n;
+    // int sum = 0;
+    // while(n>0){
+    //     int digit = n % 10;
+    //     if(digit%2 != 0){
+    //         sum += digit;
+         
+    //     }
+    //     n /= 10;
+    // }
+    // cout<<"Sum of digit is "<<sum<<endl;
+
+
+    // Print the digit of a number in reverse
     int n;
     cout<<"Enter number: ";
     cin>>n;
-    int sum = 0;
+    int rev = 0;
     while(n>0){
         int digit = n % 10;
-        if(digit%2 != 0){
-            sum += digit;
-         
-        }
+        cout<<digit;
         n /= 10;
     }
-    cout<<"Sum of digit is "<<sum<<endl;
-
     return 0;
 }
