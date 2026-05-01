548fc30624d01d51bc830e8d410e9dbbdf69aae5
ashu <ashutoshvishwakarma222@gmail.com>
2026-05-01 11:01:08 +0530

    nested if else

--- Revision/app.cpp
+++ Revision/app.cpp
@@ -116,23 +116,69 @@ int main(){
 
 
     // Check if a number is prime or not
-    int n;
-    cout<<"Enter number: ";
-    cin>>n;
-    bool isPrime = true;
-    for(int i=2;i<sqrt(n);i++){
-        if(n % i == 0){
-            isPrime = false; 
-            break;
-        }
-    }
-    if(isPrime == true){
-        cout<<n<< " is a prime number."<<endl;
-    } else {
-        cout<<n<<" is not prime number."<<endl;
-    }
-    
-    
+    // int n;
+    // cout<<"Enter number: ";
+    // cin>>n;
+    // bool isPrime = true;
+    // for(int i=2;i<sqrt(n);i++){
+    //     if(n % i == 0){
+    //         isPrime = false; 
+    //         break;
+    //     }
+    // }
+    // if(isPrime == true){
+    //     cout<<n<< " is a prime number."<<endl;
+    // } else {
+    //     cout<<n<<" is not prime number."<<endl;
+    // }
 
+
+
+// int marks;
+    // cout<<"Enter marks: ";
+    // cin>>marks;
+
+    // if(marks>=80){
+    //     cout<<"Grade A";
+    // } else if(marks>=60){
+    //     cout<<"Grade B";
+    // } else if(marks>=50){
+    //     cout<<"Grade C";
+    // } else if(marks>=45){
+    //     cout<<"Grade D";
+    // } else if(marks>=25){
+    //     cout<<"Grade E";
+    // } else {
+    //     cout<<"Fail";
+    // }
+
+
+    /*
+    Take an age from the user and then decide accordingly
+    1. If age < 18
+        print -> not eligible for job
+    2. If age >= 18,
+        print -> "eligible for job"
+    3. If age >= 55 and age <=57,
+        print -> "eligible for job, but retirement soon"
+    4. If age > 57,
+        print -> "retirement time"
+    */
+
+    int age;
+    cout<<"Enter age: ";
+    cin>>age;
+    
+    if(age < 18){
+        cout<<"Not eligible for job\n";
+    } else if(age >= 18){
+        if(age>=55 && age<=57){
+            cout<<"Eligible for job but retirement time\n";
+        } else if(age>=18 && age<55) {
+            cout<<"Eligible for job\n";
+        } else {
+        cout<<"Retirement time";
+        }
+    } 
     return 0;
 }
