a54397fadfc2f0c2eead047797596e1a54fa773a
ashu <ashutoshvishwakarma222@gmail.com>
2026-05-06 10:52:25 +0530

    added gcd code

--- BasicMaths/maths.cpp
+++ BasicMaths/maths.cpp
@@ -1,12 +1,15 @@
-#include<iostream>
-#include<math.h>
+#include <iostream>
+#include <math.h>
 using namespace std;
 
-int main(){
+int main()
+{
 
-    int n;
-    cout<<"Enter n: ";
-    cin>>n;
+    int n1, n2;
+    cout << "Enter n1: ";
+    cin >> n1;
+    cout << "Enter n2: ";
+    cin >> n2;
 
     // long long counter = 0;
     // while(n>0){
@@ -16,7 +19,6 @@ int main(){
     // }
     // cout<<counter<<endl;
 
-
     // int revNum = 0;
     // while(n>0){
     //     int lastDigit = n%10;
@@ -25,8 +27,6 @@ int main(){
     // }
     // cout<<"Reversed Number: "<<revNum<<endl;
 
-
-
     // int num = n;
     // int revNum = 0;
     // while(n>0){
@@ -34,13 +34,12 @@ int main(){
     //     revNum = 10*revNum + lastDigit;
     //     n /= 10;
     // }
-    
+
     // if(num == revNum){
     //     cout<<"It is palindrome number";
     // } else {
     //     cout<<"It is not a palindrome number.";
     // }
-
 
     // int num = n;
     // int armNum = 0;
@@ -49,13 +48,12 @@ int main(){
     //     armNum += (lastDig*lastDig*lastDig);
     //     n /= 10;
     // }
-    
+
     // if(num == armNum){
     //     cout<<"Armstrong Number"<<endl;
     // } else {
     //     cout<<"Not an armstrong number"<<endl;
     // }
-
 
     // for(int i=1;i<=sqrt(n);i++){
     //     if(n%i==0){
@@ -64,29 +62,41 @@ int main(){
     //         cout<<n<<" ";
     //     }
     //     }
-        
-    // }
 
+    // }
 
     // Check for prime number
 
-    if(n<=1){
-        cout<<n<<" is not a Prime Number."<<endl;
-    } else {
-        bool isPrime = true;
+    // if(n<=1){
+    //     cout<<n<<" is not a Prime Number."<<endl;
+    // } else {
+    //     bool isPrime = true;
 
-        for(int i=2;i<=sqrt(n);i++){
-            if(n % i == 0){
-                isPrime = false;
-                break;
-            }
+    //     for(int i=2;i<=sqrt(n);i++){
+    //         if(n % i == 0){
+    //             isPrime = false;
+    //             break;
+    //         }
+    //     }
+    //     if(isPrime == false){
+    //         cout<<n<<" is not a Prime Number."<<endl;
+    //     } else {
+    //         cout<<n<<" is a Prime Number."<<endl;
+    //     }
+    // }
+
+    // WAP to find GCD/HCF
+    while (n1 > 0 && n2 > 0){
+        if (n1 > n2){
+            n1 = n1 % n2;
+        } else{
+            n2 = n2 % n1;
         }
-        if(isPrime == false){
-            cout<<n<<" is not a Prime Number."<<endl;
-        } else {
-            cout<<n<<" is a Prime Number."<<endl;
-        }
+    } 
+    if (n1 == 0){
+        cout << n2 << " is GCD" << endl;
+    } else {
+        cout << n1 << " is GCD" << endl;
     }
-    
     return 0;
 }
