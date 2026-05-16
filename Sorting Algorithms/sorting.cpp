42f66f5ea3570fd6171c9b804eb4d549a27e0001
ashu <ashutoshvishwakarma222@gmail.com>
2026-05-16 10:52:11 +0530

    Quick Sort

--- Sorting Algorithms/sorting.cpp
+++ Sorting Algorithms/sorting.cpp
@@ -73,6 +73,30 @@ void merge_sort(int arr[], int low, int high){
     merge(arr, low, mid, high); 
 }
 
+int partition(int arr[], int low, int high){
+    int pivot = arr[low];
+    int i = low, j = high;
+    while (i < j){
+        while (arr[i] <= pivot && i <= high - 1){
+            i++;
+        }
+        while (arr[j] > pivot && j >= low + 1){
+            j--;
+        }
+        if(i<j) swap(arr[i], arr[j]);
+    }
+    swap(arr[low], arr[j]);
+    return j;
+}
+
+void quick_sort(int arr[], int low, int high){
+    if(low < high) {
+        int partitionIndex = partition(arr, low, high);
+        quick_sort(arr, low, partitionIndex-1);
+        quick_sort(arr, partitionIndex+1, high);
+    }
+}
+
 int main(){
 
     int n;
@@ -80,7 +104,7 @@ int main(){
     int arr[n];
     for(int i=0;i<n;i++) cin>>arr[i];
 
-    merge_sort(arr, 0, n-1);
+    quick_sort(arr, 0, n-1);
 
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
